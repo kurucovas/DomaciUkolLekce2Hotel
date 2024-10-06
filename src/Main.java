34c16ac6a776f44aeaa12fae7d475cbf4c22278f
kurucovas <kurucovas11@protonmail.com>
2024-10-06 23:39:11 +0200

    Dalsie Doplnenie komentaru do triedy Main
    Pridala som komentar pre publikovanie zmien

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {public static void main (String [] args) {
     reservationList.add(reservation1);
     reservationList.add(reservation2);
     reservationList.add(reservation3);
-    //cycle1:
+    //cycle1_:
     for (Booking allReservation : reservationList){System.out.println(allReservation.getStart0fReservation());}
 
 
